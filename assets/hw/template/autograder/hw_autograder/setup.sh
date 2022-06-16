194d39f412a979c668422ade75a35a93264c593d
Linh Tran <tranlm@Linhs-MacBook-Pro.local>
2022-06-16 08:34:01 -0700

    Adds hw1 & TA details

--- assets/hw/template/autograder/hw_autograder/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get update
-apt-get install -y python3.6 python3-pip python3-dev r-base
-pip3 install -r /autograder/source/tests-python/requirements.txt
-while read -r package;
-do
-  Rscript -e "install.packages('"$package"', repos='"https://ftp.osuosl.org/pub/cran/"')";
-done < "/autograder/source/tests-R/requirements.txt"
--- assets/hw/template/autograder/hw_release_sln/make_submission.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-zip -r stats202_hw1_2020.zip results/ python/ R/
